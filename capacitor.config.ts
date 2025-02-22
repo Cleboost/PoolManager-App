28f42bd0321beb7357ae5f0b02daf58845197152
Cleboost <clement.balarot@gmail.com>
2025-02-22 10:57:42 +0100

    init🎉: add initial project structure and assets

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fr.cleboost.poolmanager',
+  appName: 'Pool Manager',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ src/components/PoolRadio.vue
@@ -0,0 +1,46 @@
+<script>
+import { IonRadio } from "@ionic/vue";
+export default {
+    name: 'PoolRadio',
+    components: {
+        IonRadio,
+    },
+    props: {
+        value: {
+            type: String,
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+    },
+}
+</script>
+
+<template>
+	<div class="radioContainer">
+		<ion-radio :value="value"></ion-radio>
+		<p>{{ text }}</p>
+	</div>
+</template>
+
+<style>
+.radioContainer {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    justify-content: center;
+}
+
+.radioContainer ion-radio {
+    display: flex;
+    align-items: center;
+}
+
+.radioContainer p {
+    margin: 0;
+    display: flex;
+    align-items: center;
+}
+</style>
